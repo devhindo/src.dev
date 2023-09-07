a6e9e5284b93681f380d1c9d91216a91deef7f52
hindawi <ahmedhindawi2001@gmail.com>
2023-09-08 02:11:20 +0300

    supabase fetching done

--- server/app.js
+++ server/app.js
@@ -5,25 +5,38 @@ import './loadEnv.js'
 
 const SUPABASE_PROJECT_URL = process.env.SUPABASE_PROJECT_URL
 const SUPABASE_ANON_PUBLIC_KEY = process.env.SUPABASE_ANON_PUBLIC_KEY
+const SERVICE_ROLE_SECRET_API_KEY = process.env.SERVICE_ROLE_SECRET_API_KEY
 
-const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_PUBLIC_KEY, {auth: {persistSession: false}})
+const supabase = createClient(SUPABASE_PROJECT_URL, SERVICE_ROLE_SECRET_API_KEY, {auth: {persistSession: false}})
 
-// check if we can connect to supabase
-async function testConnection() {
-    let { data: projects, error } = await supabase
-    .from('projects')
-    .select('*')
-    console.log(error)
-    console.log(projects)
-    
-}
 
-testConnection()
-
-/*
-let { data: projects, error } = async function() { await supabase
+let { data: projects, error } = await supabase
   .from('projects')
-  .select('*')
-}()
-console.log(JSON.stringify(projects))
-*/
+  .select('name,url')
+
+  if (error) {
+    console.error(error)
+  } else {
+    console.log(projects)
+  }
+
+  const project = {
+    name: 'My Project',
+    description: 'This is my project.'
+  }
+  
+  await supabase.from('projects').insert(project)
+
+  const projectId = 1 // The ID of the project
+const rows = [
+  {
+    name: 'Row 1',
+    description: 'This is row 1.'
+  },
+  {
+    name: 'Row 2',
+    description: 'This is row 2.'
+  }
+]
+
+await supabase.from('projects').update(projectId, { rows })
