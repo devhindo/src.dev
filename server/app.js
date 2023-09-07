d36dc4f000991463c216ee133b4974cb080d49cb
hindawi <ahmedhindawi2001@gmail.com>
2023-09-08 01:56:49 +0300

    loaded env

--- server/app.js
+++ server/app.js
@@ -1,11 +1,29 @@
-const createClient = require('@supabase/supabase-js').createClient
-const dotenv = require('dotenv');
+import { createClient } from '@supabase/supabase-js'
+import './loadEnv.js'
 
-dotenv.config({path: '../.env'})
 // process.env.KEY
 
 const SUPABASE_PROJECT_URL = process.env.SUPABASE_PROJECT_URL
 const SUPABASE_ANON_PUBLIC_KEY = process.env.SUPABASE_ANON_PUBLIC_KEY
 
-const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_PUBLIC_KEY)
+const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_PUBLIC_KEY, {auth: {persistSession: false}})
 
+// check if we can connect to supabase
+async function testConnection() {
+    let { data: projects, error } = await supabase
+    .from('projects')
+    .select('*')
+    console.log(error)
+    console.log(projects)
+    
+}
+
+testConnection()
+
+/*
+let { data: projects, error } = async function() { await supabase
+  .from('projects')
+  .select('*')
+}()
+console.log(JSON.stringify(projects))
+*/
